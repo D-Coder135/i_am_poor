36138d108af56ff717767c9e498baaca26893227
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-01 00:30:49 +0530

    Add background color for the Scaffold() class

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.red[400],
         appBar: AppBar(
           backgroundColor: Colors.red[800],
           title: Center(child: Text('I Am Poor')),
