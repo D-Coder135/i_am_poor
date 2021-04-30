494dc937a49f9e0c7f6b771e360b1022dbab549e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-01 00:40:53 +0530

    Add an asset image inside the body widget of the class Scaffold()

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        body: Center(
+          child: Image(
+            image: AssetImage('images/poor.jpg'),
+          ),
+        ),
         backgroundColor: Colors.red[400],
         appBar: AppBar(
           backgroundColor: Colors.red[800],
