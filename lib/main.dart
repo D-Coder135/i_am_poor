f93be9e0243b2bc2d5e4a951c26ba56b15400333
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-01 00:25:48 +0530

    Wrap the Text() class inside the Center() class inside the title widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.red[800],
-          title: Text('I am Poor'),
+          title: Center(child: Text('I Am Poor')),
         ),
       ),
     ),
