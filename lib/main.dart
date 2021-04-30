fdd25bb4bb4a25500fe2f6084769fd2ede95d4d5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-01 00:20:21 +0530

    Add background color inside the backgroundColor widget inside the AppBar() class inside the appBar widget inside the Scaffold() class

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.red[800],
           title: Text('I am Poor'),
         ),
       ),
